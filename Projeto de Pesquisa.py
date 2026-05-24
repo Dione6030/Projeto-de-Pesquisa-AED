f161d516510c6972377e0bf58aaa4c041d5c968f
Dione Pinheiro <dionepinheiro6030@gmail.com>
2026-05-24 19:01:07 -0300

    ✨ Adiciona função com operações de conjunto

--- Projeto de Pesquisa.py
+++ Projeto de Pesquisa.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import subprocess
+import time
 
 estudantes = []
 
@@ -40,6 +41,7 @@ def ordena_jovens():
     for i, (estudante_id, (idade, genero, grau_estudo, habitacao)) in enumerate(ordenados[:10], 1):
         print(f"{i:2}. {estudante_id:12}: {idade:5}: {genero:10}: {grau_estudo:12}: {habitacao:10}")
     
+    time.sleep(5)
     limpar_tela()
     
     todos = input("Deseja ver todos? (s/n): ")
@@ -67,6 +69,7 @@ def ordena_velhos():
     for i, (estudante_id, (idade, genero, grau_estudo, habitacao)) in enumerate(ordenados[:10], 1):
         print(f"{i:2}. {estudante_id:12}: {idade:5}: {genero:10}: {grau_estudo:12}: {habitacao:10}")
     
+    time.sleep(5)
     limpar_tela()
     
     todos = input("Deseja ver todos? (s/n): ")
@@ -108,7 +111,7 @@ def comparacao_ias():
     for i, (ia, usuarios) in enumerate(ordenado, 1):
         print(f"{i:2}: {ia:23}: {usuarios:8}:")
 
-def comparacao_usuarios():
+def comparacao_usuarios_ia():
     titulo("Comparação de Usuários de IA por Gênero")
     print("N.: Gênero....: Usuários: Não-Usuários:")
     
@@ -125,18 +128,64 @@ def comparacao_usuarios():
                 else:
                     usuarios += 1
         grupo[genero] = (usuarios, non_usuarios)
-    
-    if grupo[genero] == "Male":
-        grupo[genero] = "Masculino"
-    elif grupo[genero] == "Female":
-        grupo[genero] = "Feminino"
-    elif grupo[genero] == "non-binary":
-        grupo[genero] = "Não-Binário"
-    
+
     ordenado = sorted(grupo.items(), key=lambda x: x[1][0], reverse=True)
     
     for i, (genero, (usuarios, non_usuarios)) in enumerate(ordenado, 1):
         print(f"{i:2}: {genero:10}: {usuarios:8}: {non_usuarios:12}:")
+
+
+def operacoes_com_zonas_e_graus():
+    titulo("Menu de Operções")
+    
+    alunos_rurais = set()
+    for coluna in estudantes:
+        if coluna['urban_or_rural'] == 'Rural':
+            alunos_rurais.add(coluna['student_id'])
+    alunos_urbanos = set()
+    for coluna in estudantes:
+        if coluna['urban_or_rural'] == 'Urban':
+            alunos_urbanos.add(coluna['student_id'])
+    
+    alunos_tier1 = set()
+    for col in estudantes:
+        if col['college_tier'] == 'Tier 1':
+            alunos_tier1.add(col['student_id'])
+    alunos_tier2 = set()
+    for col in estudantes:
+        if col['college_tier'] == 'Tier 2':
+            alunos_tier2.add(col['student_id'])
+    alunos_tier3 = set()
+    for col in estudantes:
+        if col['college_tier'] == 'Tier 3':
+            alunos_tier3.add(col['student_id'])
+    
+    print("1. Quantidade de alunos urbanos em cada Grau da Faculdade")
+    print("2. Quantidade de alunos rurais em cada Grau da Faculdade")
+    
+    opcao = input('Opção: ')
+    
+    if opcao == '1':
+        limpar_tela()
+        titulo('Resultados')
+        urbanos_tier1 = alunos_urbanos.intersection(alunos_tier1)
+        print(f"1º Grau: {len(urbanos_tier1)} Alunos")
+        urbanos_tier2 = alunos_urbanos.intersection(alunos_tier2)
+        print(f"2º Grau: {len(urbanos_tier2)} Alunos")
+        urbanos_tier3 = alunos_urbanos.intersection(alunos_tier3)
+        print(f"3º Grau: {len(urbanos_tier3)} Alunos")
+        time.sleep(5)
+    
+    elif opcao == '2':
+        limpar_tela()
+        titulo('Resulatado')
+        rurais_tier1 = alunos_rurais.intersection(alunos_tier1)
+        print(f"1º Grau: {len(rurais_tier1)} Alunos")
+        rurais_tier2 = alunos_rurais.intersection(alunos_tier2)
+        print(f"2º Grau: {len(rurais_tier2)} Alunos")
+        rurais_tier3 = alunos_rurais.intersection(alunos_tier3)
+        print(f"3º Grau: {len(rurais_tier3)} Alunos")
+        time.sleep(5)
 
 while True:
     titulo("Menu Principal")
@@ -159,7 +208,11 @@ while True:
     
     elif opcao == "3":
         limpar_tela()
-        comparacao_usuarios()
+        comparacao_usuarios_ia()
+    
+    elif opcao == "4":
+        limpar_tela()
+        operacoes_com_zonas_e_graus()
     
     else:
         limpar_tela()
